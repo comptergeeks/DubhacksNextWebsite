23c9e632b59ea6db02c2f41f850432a085a1d59c
Farhan Khan <comptergeeks@gmail.com>
2024-10-12 11:12:46 -0700

    website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import { LandingPage } from "../app/components/LandingBackground";
 import Link from "next/link";
 
+export const metadata = {
+  title: "DubHacks Next",
+  description: "Home",
+};
 export default function Home() {
   return (
     <div>
